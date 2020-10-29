ad06d0a8070af18beb890dd3a1eecbf60b65b0d1
RockyFu267 <rocky930323@gmail.com>
2020-10-30 00:23:29 +0800

    ADD comment

--- chatserver/main.go
+++ chatserver/main.go
@@ -74,40 +74,6 @@ func main() {
 	}
 }
 
-//Less5SecondEchoEachOther 俩启动间隔小于五秒互相通信
-func Less5SecondEchoEachOther(tmpinfo ClientInfo) {
-	time.Sleep(5 * time.Second)
-	for k := range InfoList {
-		if InfoList[k].Address != tmpinfo.Address {
-			input := bufio.NewScanner(tmpinfo.ConnChan)
-			for input.Scan() {
-				fmt.Fprintln(InfoList[k].ConnChan, "\t", strings.ToUpper(input.Text()))
-			}
-		}
-	}
-}
-
-//ReturnTime 输出时间
-func ReturnTime(c net.Conn) {
-	defer c.Close()
-	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
-		if err != nil {
-			return // e.g., client disconnected
-		}
-		time.Sleep(1 * time.Second)
-	}
-}
-
-//BecomeUpper 变大写
-func BecomeUpper(c net.Conn) {
-	defer c.Close()
-	input := bufio.NewScanner(c)
-	for input.Scan() {
-		fmt.Fprintln(c, "\t", strings.ToUpper(input.Text()))
-	}
-}
-
 //后期优化可以加其他的事件管道
 var (
 	entering = make(chan ClientChan)
@@ -152,11 +118,15 @@ func handleConn(tmpinfo ClientInfo) {
 	entering <- ch
 
 	input := bufio.NewScanner(tmpinfo.ConnChan)
+	//循环用户输入
 	for input.Scan() {
 		switch input.Text() {
+		//昵称命令
 		case "myname":
+			//判断是否已经输入过昵称
 			if infoChTmp.Name != "" {
 				infoChTmp.Ch <- "你已经输入过昵称：" + tmpinfo.Name
+				//重新循环用户输入
 				continue
 			}
 			infoChTmp.Ch <- infoChTmp.Address + ":输入昵称"
@@ -164,22 +134,29 @@ func handleConn(tmpinfo ClientInfo) {
 			if input.Scan() {
 				myname = input.Text()
 			}
+			//检查合法性
 			judge := JudgeStringSpecialSymbol(myname)
 			if judge == false {
 				infoChTmp.Ch <- infoChTmp.Address + ":昵称只支持大小写A-z以及0-9,长度不超过20"
+				//重新循环用户输入
 				continue
 			}
 			var sign bool = false
 			for k := range InfoChList {
+				//判断是否有重名
 				if InfoChList[k].Name == myname {
 					infoChTmp.Ch <- infoChTmp.Address + ":已被使用,请重新输入昵称"
+					//标记有重名
 					sign = true
+					//跳出此次循环
 					break
 				}
 			}
+			//出现重名重新循环用户输入
 			if sign == true {
 				continue
 			}
+			//一切正常 赋值
 			infoChTmp.Name = myname
 			tmpinfo.Name = myname
 			for k := range InfoChList {
@@ -192,6 +169,7 @@ func handleConn(tmpinfo ClientInfo) {
 					InfoList[k].Name = myname
 				}
 			}
+		//列出所有用户昵称命令 没有昵称也能查询
 		case "listuser":
 			var strlist []string
 			for k := range InfoChList {
@@ -199,6 +177,7 @@ func handleConn(tmpinfo ClientInfo) {
 			}
 			res2B, _ := json.Marshal(strlist)
 			infoChTmp.Ch <- tmpinfo.Name + ": " + string(res2B)
+		//列出所有组room命令 没有昵称也能查询
 		case "listroom":
 			var roomlist []string
 			for k := range RoomList {
@@ -206,7 +185,9 @@ func handleConn(tmpinfo ClientInfo) {
 			}
 			res2B, _ := json.Marshal(roomlist)
 			infoChTmp.Ch <- tmpinfo.Name + ": " + string(res2B)
+		//创建用户命令
 		case "createroom":
+			//判断是否有昵称 没有昵称不能操作
 			if infoChTmp.Name == "" {
 				infoChTmp.Ch <- tmpinfo.Address + ": " + "请先输入昵称"
 				infoChTmp.Ch <- tmpinfo.Address + ": " + help()
@@ -217,28 +198,35 @@ func handleConn(tmpinfo ClientInfo) {
 			if input.Scan() {
 				roomname = input.Text()
 			}
+			//合法性检查
 			judge := JudgeStringSpecialSymbol(roomname)
 			if judge == false {
 				infoChTmp.Ch <- infoChTmp.Name + ":房间号只支持大小写A-z以及0-9,长度不超过20"
 				continue
 			}
 			var sign bool = false
+			//检查重名
 			for k := range RoomList {
 				if RoomList[k].Name == roomname {
 					infoChTmp.Ch <- infoChTmp.Name + ":已被使用,请重试"
 					sign = true
+					//跳出此次循环
 					break
 				}
 			}
+			//出现重名重新循环用户输入
 			if sign == true {
 				continue
 			}
+			//正常赋值
 			var tmpData ChatGroup
 			tmpData.Name = roomname
 			tmpData.ChList = append(tmpData.ChList, infoChTmp)
 			RoomList = append(RoomList, tmpData)
 			infoChTmp.Ch <- infoChTmp.Name + ":房间创建成功，可通过命令listroom查看"
+		//加入房间命令
 		case "joinroom":
+			//判断是否有昵称 没有昵称不能操作
 			if infoChTmp.Name == "" {
 				infoChTmp.Ch <- tmpinfo.Address + ": " + "请先输入昵称"
 				infoChTmp.Ch <- tmpinfo.Address + ": " + help()
@@ -250,43 +238,55 @@ func handleConn(tmpinfo ClientInfo) {
 				roomname = input.Text()
 			}
 			var sign bool = false
+			//检查是否存在
 			for k := range RoomList {
 				if RoomList[k].Name == roomname {
 					for i := range RoomList[k].ChList {
 						if RoomList[k].ChList[i].Name == infoChTmp.Name {
 							infoChTmp.Ch <- infoChTmp.Name + ":你已经加入过该房间"
 							sign = true
+							//跳出房间成员的数组循环
 							break
 						}
 					}
 					if sign == true {
+						//已经在该房间 跳出检查的循环
 						break
 					}
+					//正常赋值
 					RoomList[k].ChList = append(RoomList[k].ChList, infoChTmp)
 					infoChTmp.Ch <- infoChTmp.Name + ":房间加入成功"
 					sign = true
 					break
 				}
 			}
+			//标记状态未变 不存在该room
 			if sign == false {
 				infoChTmp.Ch <- "房间不存在，可通过命令listroom查看"
 			}
 		case "help":
 			infoChTmp.Ch <- tmpinfo.Name + ": " + help()
 		default:
+			//先检查有没有昵称
 			if infoChTmp.Name == "" {
 				infoChTmp.Ch <- tmpinfo.Address + ": " + "请先输入昵称"
 				infoChTmp.Ch <- tmpinfo.Address + ": " + help()
+				//重来 判断
 				continue
 			}
+			//如果输入为空
 			if len(input.Text()) == 0 {
 				messages <- tmpinfo.Name + ": " + input.Text()
+				//重来 判断
 				continue
 			}
+			//私聊1v1
 			if string(input.Text())[0] == '@' {
+				//截取输入
 				strtmp := stringToDestinationAddr(input.Text())
 				contenttmp := stringToDestinationContent(input.Text())
 				var sign bool = false
+				//在公共管道数组里找目标管道
 				for k := range InfoChList {
 					if strtmp == InfoChList[k].Name {
 						InfoChList[k].Ch <- tmpinfo.Name + "悄悄对你说: " + contenttmp
@@ -294,45 +294,56 @@ func handleConn(tmpinfo ClientInfo) {
 						break
 					}
 				}
+				//状态未变 找不到目标管道
 				if sign == false {
 					infoChTmp.Ch <- "user not found"
 				}
+				//重来 判断
 				continue
 			}
+			//小房间私聊
 			if string(input.Text())[0] == '#' {
+				//截取
 				strtmp := stringToDestinationAddr(input.Text())
 				contenttmp := stringToDestinationContent(input.Text())
 				var sign bool = false
+				//在room数组中找目标管道
 				for k := range RoomList {
 					if strtmp == RoomList[k].Name {
 						for i := range RoomList[k].ChList {
 							RoomList[k].ChList[i].Ch <- tmpinfo.Name + "在房间" + strtmp + "小声说: " + contenttmp
 						}
 						sign = true
+						//跳出查找循环
 						break
 					}
 				}
+				//状态未变 没找到
 				if sign == false {
-					infoChTmp.Ch <- "romm rnot found"
+					infoChTmp.Ch <- "room rnot found"
 				}
+				//最后一个不需要跳出重来判断
 			} else {
 				messages <- tmpinfo.Name + ": " + input.Text()
 			}
 		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
+	//公共管道数组中删除断开的连接
 	for k := range InfoChList {
 		if InfoChList[k].Address == infoChTmp.Address {
 			InfoChList = append(InfoChList[:k], InfoChList[(k+1):]...)
 			break
 		}
 	}
+	//TCP连接数组删除断开的连接
 	for k := range InfoList {
 		if InfoList[k].Address == infoChTmp.Address {
 			InfoList = append(InfoList[:k], InfoList[(k+1):]...)
 			break
 		}
 	}
+	//房间room数组中删除断开的连接
 	for k := range RoomList {
 		for i := range RoomList[k].ChList {
 			if RoomList[k].ChList[i].Address == infoChTmp.Address {
@@ -400,6 +411,40 @@ func JudgeStringSpecialSymbol(input string) bool {
 	}
 	return true
 
+}
+
+//Less5SecondEchoEachOther 俩启动间隔小于五秒互相通信
+func Less5SecondEchoEachOther(tmpinfo ClientInfo) {
+	time.Sleep(5 * time.Second)
+	for k := range InfoList {
+		if InfoList[k].Address != tmpinfo.Address {
+			input := bufio.NewScanner(tmpinfo.ConnChan)
+			for input.Scan() {
+				fmt.Fprintln(InfoList[k].ConnChan, "\t", strings.ToUpper(input.Text()))
+			}
+		}
+	}
+}
+
+//ReturnTime 输出时间
+func ReturnTime(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+//BecomeUpper 变大写
+func BecomeUpper(c net.Conn) {
+	defer c.Close()
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		fmt.Fprintln(c, "\t", strings.ToUpper(input.Text()))
+	}
 }
 
 //PrintListName debug时候用的
