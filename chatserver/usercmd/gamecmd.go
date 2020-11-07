1204f59141f6ea57d2134935fc67d976834cdf41
RockyFu267 <rocky930323@gmail.com>
2020-11-08 01:48:42 +0800

    准备加入游戏房

---
+++ chatserver/usercmd/gamecmd.go
@@ -0,0 +1,11 @@
+package usercmd
+
+import (
+	"bufio"
+	Pt "chatserver/publictype"
+)
+
+//CreateCycles 创建石头剪刀布的房间 1v1
+func CreateCycles(infoChTmp Pt.ClientChInfo, address string, input *bufio.Scanner) {
+
+}
